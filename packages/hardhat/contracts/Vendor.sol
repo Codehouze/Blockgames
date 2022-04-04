24d6c9077463d54750409e4bc03f7124d84ba3ed
Codehouzem <codehouseinc@gmail.com>
2022-04-04 10:19:13 +0100

    challenge 2

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -5,19 +5,40 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+    event SellTokens(
+        address seller,
+        uint256 amountOfTokens,
+        uint256 amountOfETH
+    );
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    uint256 public constant tokensPerEth = 100;
+    YourToken public yourToken;
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  YourToken public yourToken;
+    // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        uint256 amountOfTokens = tokensPerEth * msg.value;
+        yourToken.transfer(msg.sender, amountOfTokens);
+        emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+    }
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() public onlyOwner {
+        uint256 totalAmount = address(this).balance;
+        address owner = msg.sender;
+        require(totalAmount > 0, "Not enough Eth available");
+        (bool success, ) = owner.call{value: totalAmount}("");
+        require(success, "Failed to withdraw Eth");
+    }
 
-  // ToDo: create a payable buyTokens() function:
-
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
-
-  // ToDo: create a sellTokens() function:
-
+    // ToDo: create a sellTokens() function:
+    function sellTokens(uint256 amount) public {
+        uint256 saleAmount = amount / tokensPerEth;
+        yourToken.transferFrom(msg.sender, address(this), amount);
+        (bool sent, bytes memory data) = msg.sender.call{value: saleAmount}("");
+        emit SellTokens(msg.sender, amount, saleAmount);
+    }
 }
